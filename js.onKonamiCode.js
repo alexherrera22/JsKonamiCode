0c820c8b6b31d9ee269cbfa7a975e54bbda661b0
alexherrera22 <alex.her.ara@gmail.com>
2015-07-12 09:43:00 -0500

    comments by redditors
    better names for functions and variables
    functions and variables are now private

--- js.onKonamiCode.js
+++ js.onKonamiCode.js
@@ -1,29 +1,28 @@
-﻿var __k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-var __t = new Array(10);
-$.fn.onKonamiCode = function (eventHandler) {
+﻿$.fn.onKonamiCode = function (eventHandler) {
+    var __konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    var __pressedKeys = new Array(10);
+    function __InsertKey(keycode) {
+        var x = 0;
+        for (x = 0; x < 9; x++) {
+            __pressedKeys[x] = __pressedKeys[x + 1];
+        }
+        __pressedKeys[9] = keycode;
+    }
+    function __CheckKeys() {
+        var ret = true;
+        for (var x = 0; x < 10; x++) {
+            if (__konamiCode[x] != __pressedKeys[x])
+                ret = false;
+        }
+        return ret;
+    }
     return this.each(function () {
         //this.keydown(
         $(this).keydown(function (e) {
-            __I(e.keyCode);
-            if (__C()) {
+            __InsertKey(e.keyCode);
+            if (__CheckKeys()) {
                 eventHandler();
             }
         });
     });
-
-}
-function __I(keycode) {
-    var x = 0;
-    for (x = 0; x < 9; x++) {
-        __t[x] = __t[x + 1];
-    }
-    __t[9] = keycode;
-}
-function __C() {
-    var ret = true;
-    for (var x = 0; x < 10; x++) {
-        if (__k[x] != __t[x])
-            ret = false;
-    }
-    return ret;
 }
